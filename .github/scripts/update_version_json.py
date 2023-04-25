bb9ff619e083a7723b8180896ddc0b7ac8a84afe
AmadeusStargazer <93411963+AmadeusStargazer@users.noreply.github.com>
2023-04-25 16:48:51 -0600

    Update update_version_json.py

--- .github/scripts/update_version_json.py
+++ .github/scripts/update_version_json.py
@@ -3,8 +3,8 @@ import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-def get_latest_app_version(package_name, api_key):
-    service = build('androidpublisher', 'v3', developerKey=api_key)
+def get_latest_app_version(package_name, credentials):
+    service = build('androidpublisher', 'v3', credentials=credentials)
     app_info = service.edits().insert(body={}, packageName=package_name).execute()
     app_edit_id = app_info['id']
 
@@ -25,8 +25,13 @@ def update_version_json(version):
         json.dump(version_data, f, indent=2)
 
 if __name__ == "__main__":
-    api_key = os.environ["API_KEY"]
     package_name = os.environ["PACKAGE_NAME"]
+    service_account_file = os.environ["SERVICE_ACCOUNT_FILE"]
 
-    latest_app_version = get_latest_app_version(package_name, api_key)
+    credentials = service_account.Credentials.from_service_account_file(
+        service_account_file,
+        scopes=["https://www.googleapis.com/auth/androidpublisher"]
+    )
+
+    latest_app_version = get_latest_app_version(package_name, credentials)
     update_version_json(latest_app_version)
